4e13987c5aef5dd963c5a08a9c3f21d262123096
Andrii Syrokomskyi <signmotion@gmail.com>
2023-11-03 17:07:17 +0100

    Added brighten() to Color extension.

--- lib/src/extensions/color_extension.dart
+++ lib/src/extensions/color_extension.dart
@@ -15,4 +15,18 @@ extension ColorStringExtension on String {
 
 extension ColorColorExtension on ui.Color {
   String get colorToArgbString => value.colorIntToArgbString;
+
+  ui.Color brighten([int percent = 12]) {
+    assert(1 <= percent && percent <= 100,
+        'Percentage must be between 1 and 100.');
+
+    final p = percent / 100;
+
+    return ui.Color.fromARGB(
+      alpha,
+      red + ((255 - red) * p).round(),
+      green + ((255 - green) * p).round(),
+      blue + ((255 - blue) * p).round(),
+    );
+  }
 }
